6dde9926bc48894006923c59053a4e57b641825a
Almir Asotic <asoica500@gmail.com>
2023-06-22 16:10:46 +0200

    Add connection string to atlas

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/amazon")
+//   .then(() => console.log("Connected to database..."))
+//   .catch((error) => console.log("Something went wrong with database", error));
 mongoose
-  .connect("mongodb://127.0.0.1:27017/amazon")
+  .connect(
+    "mongodb+srv://asotica500:elektronsko@elektronsko.gck3k6y.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => console.log("Connected to database..."))
   .catch((error) => console.log("Something went wrong with database", error));
